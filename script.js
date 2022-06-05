05ee4eed776d91efbe1582397eabb942838efb36
Roma2351 <95650797+Roma2351@users.noreply.github.com>
2022-06-06 00:44:14 +0400

    okey

--- script.js
+++ script.js
@@ -4,25 +4,42 @@ window.onload = () => {
 
 let users = []
 
-async function getData(){
 
+
+async function getData(usersCount){
+    // let inp =Number(document.querySelector('.number-requests').value)
     await axios.get('https://randomuser.me/api/?results=200').then((response)=>{
         users = response.data.results
+        
     })
-
-    render(users)
+    let inp =(document.querySelector('.number-requests').value)
+    await axios.get(`https://randomuser.me/api/?results=${inp}`).then((response)=>{
+         users= response.data.results
+        render(users,usersCount) 
+        
+        
+        
+})
 }
+          
+
+
+// const numberRequests = getData.bind(usersCount)
+// console.log(numberRequests())
 
 function render(list) {
     const blocks = document.querySelector('.blocks')
     const userCount = document.querySelector('.user-count')
     const usersAges = document.querySelector('.users-age')
+    // const usersCount = document.querySelector('.number-requests')
 
     blocks.innerHTML = ''
 
     userCount.textContent = list.length
 
     usersAges.textContent = calculateAgesSum(list)
+
+    // usersCount.textContent = calculateUsersCount(list)
 
     const fragment = document.createDocumentFragment()
     for(let user of list){
@@ -34,7 +51,7 @@ function render(list) {
         block.className = 'block shadow rounded text-center'
 
 
-        const html = `<img class="rounded user-img" src="${user.picture.large}" alt="">
+        const html = `<img class="rounded" src="${user.picture.large}" alt="">
                 <p class="user-name fs-4">${user.name.title + ' ' + user.name.first + ' ' + user.name.last}</p>
                 <p class="user-email">
                     <i class="bi bi-envelope-fill"></i>
@@ -47,7 +64,9 @@ function render(list) {
         block.innerHTML = html
 
         block.addEventListener('click', ()=>{
-            window.alert(`First Name: ${user.name.first}; Address: ${user.location.city}, ${user.location.street.name}`)
+            // window.alert(`First Name: ${user.name.first}; Address: ${user.location.city}, ${user.location.street.name}`)
+           
+            swal(` Name:${user.name.first}; /Address: ${user.location.city}, ${user.location.street.name}, /Age:${user.dob.age},/Email:${user.email}`);
         })
 
         fragment.appendChild(block)
@@ -103,4 +122,11 @@ function calculateAgesSum(users) {
         return acc + current.dob.age
     },0)
 }
+
+// function calculateUsersCount(usersCount) {
+//     return usersCount.reduce((acc, current)=> {
+//         return acc +current.email
+//     },0)
+// }
+
 

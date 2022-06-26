50ac7d6fc9fde675d49865075ae90614ea8562fa
Timur Markovnikov <gadzhibekovtimur@gmail.com>
2022-06-26 20:31:30 +0300

    Выполнил 1/2

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 window.onload = () => {
-    setTimeout(getData, 2000)
+    setTimeout(getData(100), 2000)
 }
 
 let users = []
 
-async function getData(){
+async function getData(userCount){
 
-    await axios.get('https://randomuser.me/api/?results=200').then((response)=>{
+    await axios.get(`https://randomuser.me/api/?results=${userCount}`).then((response)=>{
         users = response.data.results
     })
 
     render(users)
 }
+
+document.getElementById("inputUser").addEventListener('click', () => {
+    if (user.value > 200) return; 
+    setTimeout(getData(user.value), 2000)
+})
 
 function render(list) {
     const blocks = document.querySelector('.blocks')
@@ -32,7 +37,8 @@ function render(list) {
         // block.classList.add('rounded')
         // block.classList.add('text-center')
         block.className = 'block shadow rounded text-center'
-
+        block.setAttribute('data-bs-toggle', 'modal');
+        block.setAttribute('data-bs-target', '#exampleModal');
 
         const html = `<img class="rounded user-img" src="${user.picture.large}" alt="">
                 <p class="user-name fs-4">${user.name.title + ' ' + user.name.first + ' ' + user.name.last}</p>
@@ -47,7 +53,28 @@ function render(list) {
         block.innerHTML = html
 
         block.addEventListener('click', ()=>{
-            window.alert(`First Name: ${user.name.first}; Address: ${user.location.city}, ${user.location.street.name}`)
+            // window.alert(`First Name: ${user.name.first}; Address: ${user.location.city}, ${user.location.street.name}`)
+            const modal = document.getElementById('modal');
+            
+          modal.innerHTML = `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Информация о ${user.name.first}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+              Address: ${user.location.city},
+              ${user.location.street.name}.
+              </div>
+            </div>
+          </div>
+        </div>`;
+
+        document.querySelector('btn-close').addEventListener('click', () => {
+            modal.innerHTML = "";
+          })
+
         })
 
         fragment.appendChild(block)
@@ -95,7 +122,7 @@ function find(email) {
         return user.email === email
     }) || users
 
-    render([user] )
+    render([user])
 }
 
 function calculateAgesSum(users) {
@@ -103,4 +130,3 @@ function calculateAgesSum(users) {
         return acc + current.dob.age
     },0)
 }
-
